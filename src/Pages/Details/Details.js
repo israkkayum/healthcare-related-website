4c817b2c6f1dcdb4c1f51664b59bea2bc7df1a0d
israkkayum <israk.kayum@gmail.com>
2021-10-20 22:57:32 +0600

    second commit

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -4,8 +4,8 @@ import { useParams } from 'react-router';
 const Details = () => {
     const { serviceId } = useParams();
     return (
-          <div>
-               <h2>this is booking: {serviceId}</h2>
+          <div className="App container my-5">
+               <h2>{serviceId}</h2>
           </div>
     );
 };
